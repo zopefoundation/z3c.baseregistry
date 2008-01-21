bc7ed50f028fa4b6569c65a8d5682fbf289465f5
Roger Ineichen <roger@projekt01.ch>
2008-01-21 01:51:42 +0000

    Added dev maker

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ def read(*rnames):
 
 setup (
     name = "z3c.baseregistry",
-    version = "0.5.0",
+    version = "0.5.1dev",
     author = "Stephan Richter, Roger Ineichen and the Zope Community",
     author_email = "zope3-dev@zope.org",
     description = "Manage IComponents instances using Python code and ZCML.",
